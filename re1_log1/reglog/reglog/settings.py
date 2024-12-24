3a8fc6ddbf5933eabdb79a98aadffded6749b6cd
madara07 <madara07@itachi>
2024-12-24 07:56:30 +0530

    Initial commit

--- re1_log1/reglog/reglog/settings.py
+++ re1_log1/reglog/reglog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k2&f#y1rov@k+_z(m9q())xv%(uzqqsf6yesmr$pgu131ucbfv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
